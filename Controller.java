410435ed7b1cc959e20550eaba8df6feb423750e
SOPHANITH <yoongsophanith18@gmail.com>
2026-06-24 15:29:51 +0700

    xx

---
+++ Controller.java
@@ -0,0 +1,155 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    @FXML
+    private Button buttoBackspace;
+
+    @FXML
+    private Button button0;
+
+    @FXML
+    private Button button1;
+
+    @FXML
+    private Button button2;
+
+    @FXML
+    private Button button3;
+
+    @FXML
+    private Button button4;
+
+    @FXML
+    private Button button5;
+
+    @FXML
+    private Button button6;
+
+    @FXML
+    private Button button7;
+
+    @FXML
+    private Button button8;
+
+    @FXML
+    private Button button9;
+
+    @FXML
+    private Button buttonAdd;
+
+    @FXML
+    private Button buttonClear;
+
+    @FXML
+    private Button buttonDivide;
+
+    @FXML
+    private Button buttonDot;
+
+    @FXML
+    private Button buttonEquals;
+
+    @FXML
+    private Button buttonMultiply;
+
+    @FXML
+    private Button buttonSubtract;
+
+    @FXML
+    private Label label_result;
+
+    @FXML
+    void on_Add(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_Backspace(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_Clear(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_Digit0(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_Digit1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_Digit2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_Digit3(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_Digit4(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_Digit5(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_Digit6(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_Digit7(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_Digit8(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_Digit9(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_Divide(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_Dot(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_Equals(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_Multiply(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_Subtract(ActionEvent event) {
+
+    }
+
+}

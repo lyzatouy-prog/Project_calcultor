1f945c6a9e1d9a3fef054ae4edf8cf2dc486b71c
SOPHANITH <yoongsophanith18@gmail.com>
2026-06-24 15:49:42 +0700

    ss

--- Main.java
+++ Main.java
@@ -1,3 +1,16 @@
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
     
 }

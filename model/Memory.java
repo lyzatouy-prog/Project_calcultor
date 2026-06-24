a99276b9378ba4baac46d18be25af7a6de6608a8
SOPHANITH <yoongsophanith18@gmail.com>
2026-06-24 15:48:04 +0700

    xx

--- model/Memory.java
+++ model/Memory.java
@@ -1,9 +1,42 @@
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+package model;
 
-public class Memory extends Applications{
-    
+public class Memory {
+
+    static String first_variable;
+    static String operator;
+    static String second_variable;
+
+    public void set_first_variable(String first_variable){
+        Memory.first_variable = first_variable;
+    }
+
+    public String get_first_variable(){
+        return first_variable;
+    }
+
+        public void set_operator(String operator){
+        Memory.operator = operator;
+    }
+
+    public String get_operator(){
+        return operator;
+
 }
-//jj
+
+    public void set_second_variable(String second_variable){
+        Memory.second_variable = second_variable;
+    }
+
+    public String get_second_variable(){
+        return second_variable;
+
+}
+
+    public static void main(String[] args) {
+        Memory memory = new Memory();
+        memory.set_first_variable(first_variable: "5");
+        System.out.println(memory.get_first_variable());
+        
+    }
+
+}
